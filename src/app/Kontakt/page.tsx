522284c7d81adf1882320b90616f5a8d07b04e4e
ge78fug <ge78fug@mytum.de>
2024-01-05 09:56:54 +0100

    Update New Home

--- src/app/Kontakt/page.tsx
+++ src/app/Kontakt/page.tsx
@@ -4,10 +4,10 @@ import KontaktFormular from '../components/KontaktFormular';
 export default function Kontakt() {
   return (
     <div className='flex flex-col gap-16 m-5'>
-      <section className='bg-neutral-50 flex flex-col md:flex-row p-10 md:p-15 lg:p-20 gap-6 md:gap-8 lg:gap-10 rounded-lg'>
-        <div className='flex flex-col gap-6 md:gap-8 lg:gap-10 flex-1'>
-          <h2 className='text-xl md:text-2xl lg:text-3xl tracking-widest leading-normal'>Kontakt</h2>
-          <p className='text-xs md:text-s lg:text-base tracking-widest'>Schreiben Sie uns Ihr Anliegen und wir melden uns bei Ihnen innerhalb von 1 - 2 Werktagen zurück.</p>
+      <section className='bg-neutral-50 flex flex-col md:flex-row p-10 md:p-15 lg:p-20 gap-8 md:gap-10 lg:gap-12 rounded-lg'>
+        <div className='flex flex-col gap-6 md:gap-8 lg:gap-10'>
+          <h2 className='text-2xl md:text-3xl lg:text-4xl tracking-widest leading-normal'>Kontakt</h2>
+          <p className='text-base md:text-lg lg:text-xl tracking-widest'>Schreiben Sie uns Ihr Anliegen und wir melden uns bei Ihnen innerhalb von 1 - 2 Werktagen zurück.</p>
         </div>
         <KontaktFormular />
       </section>
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -51,7 +51,7 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className="flex items-stretch justify-between md:justify-start gap-8 md:gap-14 lg:gap-20 p-4 shadow-md bg-neutral-50">
+      <header className="flex items-center justify-between lg:justify-start gap-8 md:gap-14 lg:gap-20 p-5 shadow-md bg-neutral-50 text-base md:text-lg lg:text-xl">
         <div className="logo flex flex-shrink-0 items-center">
           <Image
               src="/logos/VA-Logo.png" 
@@ -62,7 +62,7 @@ const Header: React.FC = () => {
               className="w-6 md:w-8 lg:w-10" />
         </div>
         
-        <ul className="hidden md:flex items-center gap-4 md:gap-6 lg:gap-8 h-full">
+        <ul className="hidden lg:flex items-center gap-4 md:gap-6 lg:gap-8 h-full">
           {navigationLinks.map((link) => {
             if (link.title === 'Immobilien kaufen') {
               return <DropdownMenu key={link.title} title={link.title} path={link.path} links={kaufLinks} />;
@@ -73,7 +73,7 @@ const Header: React.FC = () => {
             } else {
             return (
                 <li key={link.title}>
-                  <Link href={link.path} className="text-gray-600 hover:text-gray-900 transition-colors text-xs md:text-sm lg:text-base">
+                  <Link href={link.path} className="text-gray-600 hover:text-gray-900 transition-colors text-lg md:text-xl lg:text-xl">
                     {link.title}
                   </Link>
                 </li>
@@ -85,7 +85,7 @@ const Header: React.FC = () => {
         <button
           title='Menu'
           type='button'
-          className="md:hidden"
+          className="lg:hidden"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <svg height="16" width="14" viewBox="0 0 448 512">
