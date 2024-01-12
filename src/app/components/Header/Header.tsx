bc06ed9505afedbfed6df82dd8c423523e8cc140
ge78fug <ge78fug@mytum.de>
2024-01-12 13:57:14 +0100

    Header Change

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import React from 'react';
 import DropdownMenu from './DropdownMenu';
 import MobileMenu from './MobileMenu';
@@ -10,16 +10,17 @@ import MobileMenu from './MobileMenu';
 type NavLink = {
   title: string;
   path: string;
+  id?: string;
 };
 
 const navigationLinks: NavLink[] = [
-  { title: 'Home', path: '/' },
-  { title: 'Über Valuvis', path: '/UeberValuvis' },
-  { title: 'Immobilienmarkt', path: '/Immobilienmarkt' },
-  { title: 'Immobilien kaufen', path: '/Immobilien-kaufen' },
-  { title: 'Immobilien verkaufen', path: '/Immobilien-verkaufen' },
-  { title: 'Dienstleistungen', path: '/Dienstleistungen' },
-  { title: 'Mehr', path: '/Mehr' }
+  { title: 'Home', path: '/', id: 'Home' },
+  { title: 'Über Valuvis', path: '/UeberValuvis', id: 'UeberValuvis' },
+  { title: 'Immobilienmarkt', path: '/Immobilienmarkt', id: 'Immobilienmarkt' },
+  { title: 'Immobilien kaufen', path: '/Immobilien-kaufen', id: 'Immobilienkaufen' },
+  { title: 'Immobilien verkaufen', path: '/Immobilien-verkaufen', id: 'Immobilienverkaufen' },
+  { title: 'Dienstleistungen', path: '/Dienstleistungen', id: 'Dienstleistungen' },
+  { title: 'Mehr', path: '/Mehr', id: 'Mehr' }
 ];
 
 const kaufLinks: NavLink[] = [
@@ -46,6 +47,33 @@ const moreLinks: NavLink[] = [
 const Header: React.FC = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [hoveredLink, setHoveredLink] = useState('');
+  const [linkPosition, setLinkPosition] = useState({ left: 0, width: 0 });
+  const [dropdownContent, setDropdownContent] = useState('');
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (linkRef.current) {
+      const rect = linkRef.current.getBoundingClientRect();
+      setLinkPosition({ left: rect.left, width: rect.width });
+    }
+  }, [hoveredLink]);
+
+  useEffect(() => {
+    if ((hoveredLink === 'Immobilien kaufen' || hoveredLink === 'Immobilien verkaufen' || hoveredLink === 'Mehr') && divRef.current) {
+      const linkElement = document.querySelector(`#${hoveredLink.replace(' ', '')}`);
+      if (linkElement) {
+        const rect = linkElement.getBoundingClientRect();
+        divRef.current.style.left = `${rect.left}px`;
+      }
+    }
+  }, [hoveredLink]);
+
+  const handleMouseEnter = (linkTitle: string) => {
+    setHoveredLink(linkTitle);
+    setDropdownContent(linkTitle);
+  };
 
   const handleCloseMenu = () => {
     setIsMenuOpen(false);
@@ -53,47 +81,61 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className={`fixed top-0 left-0 z-50 w-full flex items-center justify-between lg:justify-start gap-8 md:gap-14 lg:gap-20 p-4 shadow-md bg-transparent text-sm md:text-base lg:text-base backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white`}>
-        <div className="logo flex flex-shrink-0 items-center">
-          <Image
-              src="/logos/VA-Logo.png" 
-              alt="Valuvis Logo"
-              width={30}
-              height={22}
-              layout='fixed'
-              className="w-4 md:w-6 lg:w-8" />
-        </div>
+      <header className={`fixed top-0 left-0 z-50 w-full shadow-md bg-transparent text-sm md:text-base lg:text-base`}>
+        <nav className='hidden lg:flex peer px-4 h-auto w-full items-center justify-between lg:justify-start gap-8 md:gap-14 lg:gap-10 xl:gap-20 backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white'>
+          <div className="logo flex flex-shrink-0 items-center">
+            <Image
+                src="/logos/VA-Logo.png" 
+                alt="Valuvis Logo"
+                width={30}
+                height={22}
+                layout='fixed'
+                className="w-4 md:w-6 lg:w-8" />
+          </div>
+          
+          <ul className="hidden lg:flex items-center gap-4 md:gap-6 lg:gap-2 xl:gap-4 h-auto">
+            {navigationLinks.map((link) => (
+              <li key={link.title} className='peer py-4'
+                  onMouseEnter={() => handleMouseEnter(link.title)}
+                  onMouseLeave={() => setHoveredLink('')}>
+                <Link id={link.id} href={link.path} className="hover:text-gray-900 transition-colors lg:px-2 xl:px-3"
+                  >
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
         
-        <ul className="hidden lg:flex items-center gap-4 md:gap-6 lg:gap-8 h-full">
-          {navigationLinks.map((link) => {
-            if (link.title === 'Immobilien kaufen') {
-              return <DropdownMenu key={link.title} title={link.title} path={link.path} links={kaufLinks} />;
-            } else if (link.title === 'Immobilien verkaufen') {
-              return <DropdownMenu key={link.title} title={link.title} path={link.path} links={verkaufLinks} />;
-            } else if (link.title === 'Mehr') {
-              return <DropdownMenu key={link.title} title={link.title} path={link.path} links={moreLinks} />;
-            } else {
-            return (
-                <li key={link.title}>
-                  <Link href={link.path} className="hover:text-gray-900 transition-colors">
-                    {link.title}
-                  </Link>
-                </li>
-              );
-            }
-          })}
-        </ul>
-        
-        <button
-          title='Menu'
-          type='button'
-          className="lg:hidden"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          <svg height="16" width="14" viewBox="0 0 448 512">
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
-          </svg>
-        </button>
+        <DropdownMenu 
+          divRef={divRef}
+          dropdownContent={dropdownContent} 
+          kaufLinks={kaufLinks} 
+          verkaufLinks={verkaufLinks} 
+          moreLinks={moreLinks} 
+        />
+      
+      <nav className='flex lg:hidden peer p-4 h-auto w-full items-center justify-between lg:justify-start gap-8 md:gap-14 lg:gap-10 xl:gap-20 backdrop-filter backdrop-blur-lg bg-opacity-30 bg-white'>
+          <div className="logo flex flex-shrink-0 items-center">
+            <Image
+                src="/logos/VA-Logo.png" 
+                alt="Valuvis Logo"
+                width={30}
+                height={22}
+                layout='fixed'
+                className="w-4 md:w-6 lg:w-8" />
+          </div>
+          <button
+            title='Menu'
+            type='button'
+            className="lg:hidden"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <svg height="16" width="14" viewBox="0 0 448 512">
+              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+            </svg>
+          </button>
+        </nav>
       </header>
       <MobileMenu isOpen={isMenuOpen} onClose={handleCloseMenu} navigationLinks={navigationLinks} kaufLinks={kaufLinks} verkaufLinks={verkaufLinks} moreLinks={moreLinks} />
     </>
--- src/app/components/Header/MobileMenu.tsx
+++ src/app/components/Header/MobileMenu.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import DropdownMobileMenu from './DropdownMobileMenu';
 
 interface MobileMenuProps {
@@ -29,17 +30,29 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, navigationLinks, onClos
     }
 
     return (
-        <div className={`absolute flex flex-col w-full h-full lg:hidden bg-white overflow-y-hidden z-50 `}>
-          <button
-            title='Menu'
-            type='button'
-            className='self-end p-4'
-            onClick={onClose}
-          >
-            <svg height="16" width="14" viewBox="0 0 448 512">
-              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
-            </svg>
-          </button>
+        <div className={`fixed top-0 left-0 flex flex-col gap-4 w-full h-full lg:hidden bg-white overflow-y-hidden z-50`}>
+          <div className="flex justify-between items-center border-b border-gray-200">
+      
+            <div className="logo flex flex-shrink-0 items-center p-4">
+              <Image
+                  src="/logos/VA-Logo.png" 
+                  alt="Valuvis Logo"
+                  width={30}
+                  height={22}
+                  layout='fixed'
+                  className="w-4 md:w-6 lg:w-8" />
+            </div>
+            <button
+              title='Menu'
+              type='button'
+              className='self-end p-4'
+              onClick={onClose}
+            >
+              <svg height="16" width="14" viewBox="0 0 448 512">
+                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+              </svg>
+            </button>
+          </div>
           <ul className='flex flex-col gap-5 w-full pl-5'>
             {navigationLinks.map((link) => {
               if (link.title === 'Immobilien kaufen') {
